da09e0c6e23542c41964e30dd34062c12b4caf8c
J.Cole <ccjohn015@gmail.com>
2022-02-13 17:51:02 -0600

    added starter function

--- index.js
+++ index.js
@@ -8,3 +8,60 @@ const fs = require('fs');
 const generatePage = require('./src/page-template');
 
 const employees = [];
+
+function managerQuestions() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("\n" + "Please enter manager's name.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'managerId',
+            message: "What is the team manager's id number?",
+            validate: idNumber => {
+                if (idNumber) {
+                    return true;
+                } else {
+                    console.log("\n" + "Please enter manager's id number.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?",
+            validate: emailInput => {
+                if (emailInput.includes("@")) {
+                    return true;
+                } else {
+                    console.log("\n" + "Please enter a valid email.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'managerOfficeNum',
+            message: "What is the team manager's office number?",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log("\n" + "Please enter manager's office number.")
+                    return false;
+                }
+            }
+        }
+    ])
+}
