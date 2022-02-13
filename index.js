c1642eb0a804ca26c9eac29d49573468085dc4ca
J.Cole <ccjohn015@gmail.com>
2022-02-13 17:49:23 -0600

    started index page

---
+++ index.js
@@ -0,0 +1,10 @@
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const employees = [];
